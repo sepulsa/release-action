0fedffd70fbcf2379ecc9d513bc99b18d289f540
Abi <abi@alterra.id>
2021-04-09 16:40:38 +0700

    Remove unused variables

--- __tests__/environment.test.ts
+++ __tests__/environment.test.ts
@@ -2,14 +2,10 @@ import {deleteEnvironment} from '../src/environment'
 
 test('Delete invalid environment', async () => {
   const key = 'JIRA-404'
-  const token = process.env.GITHUB_TOKEN as string
-
   expect(await deleteEnvironment('', key)).toEqual(404)
 })
 
 test('Delete environment', async () => {
   const key = 'JIRA-204'
-  const token = process.env.GITHUB_TOKEN as string
-
   expect(await deleteEnvironment('', key)).toEqual(204)
 })
